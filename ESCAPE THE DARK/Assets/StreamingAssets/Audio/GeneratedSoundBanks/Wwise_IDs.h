0a6910de19531b7c34f8f187f069fb2ef761a70c
mikeloganaudio <65549342+mikeloganaudio@users.noreply.github.com>
2021-10-17 12:42:35 +0100

    Ambient Zones and fixed Audioemitter Script

---
+++ ESCAPE THE DARK/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_BABY_MOBILE = 937063987U;
+        static const AkUniqueID PLAY_BLIND___WINDOW_2 = 1660469979U;
+        static const AkUniqueID PLAY_DOOR_CREAKING = 3327054553U;
+        static const AkUniqueID PLAY_FRIDGE_HUM = 3365485150U;
+        static const AkUniqueID PLAY_KITCHEN_ROOM_TONE_CLOCK_FREEZER_AMBIX = 1621347150U;
+        static const AkUniqueID PLAY_ROOM_TONE_LIGHT_HOUSEHOLD_2 = 3627614682U;
+        static const AkUniqueID PLAY_WOOD_CREAKING_1 = 2615280544U;
+        static const AkUniqueID STOP_FRIDGE_HUM = 2470329820U;
+        static const AkUniqueID STOP_KITCHEN_ROOM_TONE_CLOCK_FREEZER_AMBIX = 1905864408U;
+        static const AkUniqueID STOP_ROOM_TONE_LIGHT_HOUSEHOLD_2 = 1042786396U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID RESONANCEAUDIO = 1129712971U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
